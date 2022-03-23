c3b51c913ca0183df78fd2675f7d9af9d895b896
umeda <umeda0213@gmail.com>
2022-03-23 15:44:59 +0800

    2022-03-23

--- app/solana-nft-collaterized-loans/pages/my_loans.tsx
+++
@@ -1,6 +0,0 @@
-const MyLoans = () => {
-    return (
-        <div>MyLoans</div>
-    );
-}
-export default MyLoans;
--- app/solana-nft-collaterized-loans/utils/consts.ts
+++ app/solana-nft-collaterized-loans/utils/consts.ts
@@ -1,4 +1,7 @@
 export const PROGRAM_ID = '7Ags9aVzdyCP2SALzWhgRJG8VRAsPiAs5wGCTEn2PMTr';
 export const STABLE_COIN_KEY = 'Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr';
-export const NFT_LOAN_KEY = 'J44HeGXA2Dw7YTJDcf5hqjmxrHHhPMM47447pvs2CEY5';
-export const USDC_VAULT_KEY = 'G6i1PY47JuS1AWrQecWLHtDas8boxrhtKqjskFL2oeTv';
+
+export const CONFIG_PDA_SEED = "config";
+export const STABLE_COIN_PDA_SEED = "stable";
+export const NFT_PDA_SEED = "nft";
+export const ORDER_PDA_SEED = "order";
